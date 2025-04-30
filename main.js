eb4bbd41620f0a737ba8e188138c78ccdd517b68
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-04-30 11:28:06 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,64 +1,76 @@
 // set up character pairs
 
-// define accepted characters
-let char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// define accepted characters, ex: abc[special characters]ABC
+let GSChar = 'abcdefghijklmnopqrstuvwxyz !ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // convert into array
-let options = char.slice('');
+GSChar = GSChar.split('');
 // array for every possible character pair
-let combos = [];
+let GSCombos = [];
 // array for single characters
-let oneletter = [];
+let GSUnicode = [[], []];
+// just to make the code a little smaller
+const GSCharNumber = GSChar.length - 26;
 // generate character pairs
 function create() {
-    for (let i = 0; i < 26; i++) {
-        for (let x = 0; x < 26; x++) {
-            combos[combos.length] = options[i] + options[x + 26];
+        for (let i = 0; i < GSCharNumber; i++) {
+            for (let x = 0; x < GSCharNumber; x++) {
+                GSCombos.push(GSChar[i] + GSChar[x + 26]);
+            };
+        };
+        for (let i = 0; i < GSCharNumber; i++) {
+            for (let x = 0; x < GSCharNumber; x++) {
+                GSCombos.push(GSChar[x + 26] + GSChar[i]);
+            };
+        };
+        for (let i = 0; i < GSCharNumber; i++) {
+            for (let x = 0; x < GSCharNumber; x++) {
+                GSCombos.push(GSChar[i] + GSChar[x]);
+            };
+        };
+        for (let i = 0; i < GSCharNumber; i++) {
+            for (let x = 0; x < GSCharNumber; x++) {
+                GSCombos.push(GSChar[i + 26] + GSChar[x + 26]);
+            };
         };
     };
-    for (let i = 0; i < 26; i++) {
-        for (let x = 0; x < 26; x++) {
-            combos[combos.length] = options[x + 26] + options[i];
-        };
-    };
-    for (let i = 0; i < 26; i++) {
-        for (let x = 0; x < 26; x++) {
-            combos[combos.length] = options[i] + options[x];
-        };
-    };
-    for (let i = 0; i < 26; i++) {
-        for (let x = 0; x < 26; x++) {
-            combos[combos.length] = options[i + 26] + options[x + 26];
-        };
-    };
-};
-// convert pairs into one character, characters from https://raw.githubusercontent.com/bits/UTF-8-Unicode-Test-Documents/refs/heads/master/UTF-8_sequence_unseparated/utf8_sequence_0-0xffff_assigned_printable_unseparated.txt
+// characters from https://raw.githubusercontent.com/bits/UTF-8-Unicode-Test-Documents/refs/heads/master/UTF-8_sequence_separated/utf8_sequence_0-0x10ffff_assigned_printable.txt
 function convert() {
-    for (let i = 0; i < combos.length; i++) {
-        oneletter[i] = unicodeChars.charAt(i);
+        for (let i = 0; i < GSCombos.length; i++) {
+            GSUnicode[0].push(GSUnicodeData.at(i));
+            GSUnicode[1].push(GSCombos[i]);
+        };
+     // alert(GSUnicode[0][0])
     };
-};
 create();
 convert();
 
-// encode
-// string to store input
-let sampleInput = "Hello World";
-// remove spaces (temp)
-sampleInput = sampleInput.replaceAll(' ', '');
-// tring to store converted input
-let sampleAdjusted = '';
-function encodeString() {
+// encoding
+function GSEncode(input) {
+// string to store output
+        let output = '';
 // repeatedly add the converted pair to the end of sampleAdjusted
-    for (let i = 0; i < Math.floor(sampleInput.length / 2); i++) {
-        sampleAdjusted = sampleAdjusted.concat('', oneletter[combos.indexOf(sampleInput.slice('')[i * 2] + sampleInput.slice('')[(i * 2) + 1])]);
+        for (let i = 0; i < Math.floor(input.length / 2); i++) {
+            output = output.concat('', GSUnicode[0][GSCombos.indexOf(input.at(i * 2) + input.at((i * 2) + 1))]);
+        };
+// check if even or odd
+        let f = JSON.stringify(input.length / 2).at(-1);
+        if (f % 2 !== 0) {
+            output = output.concat('', GSUnicode[0][GSCombos.indexOf(input.at(-1) + ' ')]);
+        };
+        return output;
     };
-// check if even or odd (I forgot how to do it quickly)
-    let f = JSON.stringify(sampleInput.length).at(-1);
-    if (f == 1 || f == 3 || f == 5 || f == 7 || f == 9) {
-        sampleAdjusted = sampleAdjusted.concat('', sampleInput.at(-1));
-    };
-    navigator.clipboard.writeText(sampleAdjusted);
-    window.alert(sampleAdjusted);
-};
 
-encodeString();
+// decoding
+function GSDecode(input) {
+// string to store output
+        let output = '';
+// repeatedly add the converted pair to the end of sampleAdjusted
+        for (let i = 0; i < input.length; i++) {
+            output = output.concat('', GSUnicode[1][GSUnicode[0].indexOf(input.at(i))]);
+        };
+        if (output.at(-1) == ' ') {
+            output.split(0, -1);
+        }
+        return output;
+    };
+window.alert(GSDecode(GSEncode('Hello World! ')));
